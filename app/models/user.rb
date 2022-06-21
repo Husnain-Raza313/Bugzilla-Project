81c01a0de8f214441ccad9168b89026efc1eac14
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-06-22 02:06:59 +0500

    Application: Changings in  Devise * Added Devise Views * Added Extra User_type and Name Fields in Devise *Added Home Controller With Index Action and View *Changed Routes.rb file for devise views to show on root page * Changed Permit Parameters for devise in Application_Controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum user_type: [:user, :manager, :developer, :qa]
+
+  after_initialize :set_default_user_type, :if => :new_record?
+
+  def set_default_user_type
+    self.user_type ||= :user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  # get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+
+    end
+  end
 end
