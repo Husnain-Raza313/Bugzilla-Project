61578c5902e483beb3a221ef6a56e76404bc458d
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-07-05 17:02:42 +0500

    DatabaseChanged: Changed SQLite to Postgresql * Changed Settings in database.yml * Added enable_extension line in schema.rb *Added Gem pg

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+# gem for Postgresql
+gem 'pg'
+
+
 # adding jquery
 gem 'jquery-rails'
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+enable_extension "plpgsql"
 
 ActiveRecord::Schema.define(version: 2022_07_04_174001) do
 
