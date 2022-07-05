0f3a40464a3ea370d7a900d9dbc2a9b4f401984f
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-07-05 22:51:20 +0500

    CodePieceModel: Added prefix piece_status to piece_status enums keys * To avoid the problem of using reserved words like new in keys

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,5 +1,5 @@
 class Bug < CodePiece
-  enum piece_status: [:new, :started, :resolved]
+  enum piece_status: [:new, :started, :resolved], _prefix: :piece_status
   belongs_to :project
   validates :piece_status, inclusion: { in: [:new, :started, :resolved] }
 
