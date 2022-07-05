7d6c9ff79b0fac72d523984e6810e01e60c71e0f
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-07-05 18:52:41 +0500

    Production.rb: Changed uglifier to Uglifier.new(harmony: true) * because it showed an error while deploying it on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_07_04_174001) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "code_pieces", force: :cascade do |t|
-    t.integer "project_id"
+    t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title", default: "", null: false
@@ -33,8 +34,8 @@ ActiveRecord::Schema.define(version: 2022_07_04_174001) do
   end
 
   create_table "user_projects", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "project_id"
+    t.bigint "user_id"
+    t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_user_projects_on_project_id"
@@ -55,4 +56,5 @@ ActiveRecord::Schema.define(version: 2022_07_04_174001) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "code_pieces", "projects"
 end
