5ecf15a2f28712978015861db1e22994cff3a715
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-07-07 02:39:15 +0500

    Home: Added Partials and CSS * Partials: Dev, QA, Manager Panels and User Profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+
+   if current_user.manager?
+    puts "Manager"
+  elsif current_user.developer?
+    puts "Developer"
+  elsif current_user.qa?
+    puts "QA"
+  elsif current_user.user?
+    puts "user"
+  end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     authenticated :user do
       root 'home#index', as: :authenticated_root
-
     end
 
     unauthenticated do
@@ -13,4 +12,5 @@ Rails.application.routes.draw do
 
     end
   end
+
 end
