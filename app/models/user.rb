6759eb0c4b46269c2ca3cd3af36baf163c586ff4
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-07-20 02:35:13 +0500

    Pundit Gem and Model: Installed Pundit Gem and added dependent destroy Options for Projects and Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 
   # has_many :code_pieces, dependent: :nullify
-  has_many :code_piece_users
+  has_many :code_piece_users, dependent: :destroy
 
-  has_many :user_projects
+  has_many :user_projects, dependent: :destroy
   has_many :projects , through: :user_projects
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
