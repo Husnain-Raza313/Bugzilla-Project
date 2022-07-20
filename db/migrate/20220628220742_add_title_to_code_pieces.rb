6a64b32532fbfe69c6308d85dbdd34faf1b9a6a4
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-07-21 01:47:28 +0500

    Migrations: Removed Useless Migrations

--- db/migrate/20220628220742_add_title_to_code_pieces.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddTitleToCodePieces < ActiveRecord::Migration[5.2]
-  def change
-    add_column :code_pieces, :title, :string, null: false, default: ''
-    add_index :code_pieces, :title, unique: true
-  end
-end
