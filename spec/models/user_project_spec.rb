f2789c47d919c755feb5c27ee7f63b247eba34c9
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-08-15 01:39:27 +0500

    UserProjectSpec: Added UserProject spec file for testing

---
+++ spec/models/user_project_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe UserProject, type: :model do
+  describe 'Associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:project) }
+  end
+end
