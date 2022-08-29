030a04f08e8a7b971c50714848c74f5b82c32127
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-08-29 13:22:59 +0500

    first commit

---
+++ react-bugzilla-project/react-bugzilla/src/components/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div class="container-fluid bg-warning bar">
+    <h1 className='text-dark text-decoration-none'>Bugzilla</h1>
+
+  </div>
+  )
+}
+
+export default Topbar
