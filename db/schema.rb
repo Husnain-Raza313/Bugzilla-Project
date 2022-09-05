15eb6e20a8df003cd4e0c2b74de0b2e9bfb195f9
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-09-05 16:51:50 +0500

    Schema: Rollbacked all the changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_085747) do
+ActiveRecord::Schema.define(version: 2022_08_08_055402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,15 +62,6 @@ ActiveRecord::Schema.define(version: 2022_09_05_085747) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.integer "user_type", default: 0, null: false
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.boolean "allow_password_change", default: true
-    t.json "tokens"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
