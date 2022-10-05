cb9a975289014f31e50abe0d274b73db6a3463e5
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-10-05 17:12:35 +0500

    PaymentIntents and Bugs.js.erb: Added ErrorHandling

--- app/controllers/payment_intents_controller.rb
+++ app/controllers/payment_intents_controller.rb
@@ -1,12 +1,27 @@
-class PaymentIntentsController < ActionController::API
+class PaymentIntentsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def create
     bug=Bug.find(params[:id].to_i)
-    paymentIntent= Stripe::PaymentIntent.create(
-      {
-        amount: bug.bug_price.to_i,
-        currency: 'usd',
-      },
-    )
-    render json: paymentIntent
+    begin
+      paymentIntent= Stripe::PaymentIntent.create(
+        {
+          amount: bug.bug_price.to_i,
+          currency: 'usd'
+        },
+      )
+      render json: paymentIntent
+    rescue Stripe::CardError => e
+      puts "A payment error occurred: #{e.error.message}"
+      render json: e.error
+    rescue Stripe::InvalidRequestError => e
+      p "Here"
+      render json: e.error
+    rescue Stripe::StripeError => e
+      puts "Another problem occurred, maybe unrelated to Stripe. #{e.error.message}"
+      render json: e.error
+    end
+
   end
 end
