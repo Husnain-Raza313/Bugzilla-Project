01937ad110a84180cb268e3f4306a9f13aa6fbd1
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2022-10-27 14:57:26 +0500

    SubscriptionCheckoutController: Removed Additional Comments

--- app/controllers/subscription_checkout_controller.rb
+++ app/controllers/subscription_checkout_controller.rb
@@ -25,21 +25,9 @@ class SubscriptionCheckoutController < ApplicationController
     current_user.update(
       subscription_status: 'Canceled',
     )
-    # respond_to do |format|
-    #       format.html { redirect_to authenticated_root_url, flash: { success: 'Successfully Unsubscribed and Status Updated.' } }
-    #     end
+
     redirect_to subscription_checkout_index_path , notice: "Successfully Unsubscribed and Status Updated"
-    # render json: { message: 'success' }
-    # puts "deleted object : #{deleted_subscription}"
-    # flash[:success] = "successfully updated"
-    # render 'users/index' and return
-    # respond_to do |format|
-    #   # if deleted_subscription
-    #   #   format.html { render 'subscription_checkout/edit', status: :unprocessable_entity }
-    #   # else
-    #     format.html { redirect_to authenticated_root_url, flash: { success: 'Successfully Unsubscribed and Status Updated.' } }
-    #   # end
-    # end
+
 
   end
 
